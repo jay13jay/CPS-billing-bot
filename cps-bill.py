f51ae4d8bc33ec95c613b84a79dbab4fa84294f8
Josh <josh.hackney13@gmail.com>
2021-03-08 15:55:42 -0500

    seperated cookies and params and formatted the request

--- cps-bill.py
+++ cps-bill.py
@@ -1,19 +1,44 @@
-#/usr/bin/env python3
-
 import requests
 
+cpsUID                = "REPLACEWITHUSERID"
 startDate             = "2021-01-29"
 endDate               = "2021-02-28"
 timeframe             = "quarter_hour"
 enhancedBilling       = "false"
 includeRegisterData   = "false"
-endpointURL           = 'https://cps.opower.com/ei/edge/apis/DataBrowser-v1/cws/utilities/cps/utilityAccounts/GUID/reads?'
-urlOpts               = 'startDate=%s&endDate=%s&aggregateType=quarter_hour&includeEnhancedBilling=false&includeMultiRegisterData=false'
+userAgent             = "Checking my usage, bro, don't taze me"
+language              = "en-US"
+oPowerEntities        = ["urn:opower:customer:uuid:"+cpsUID]
 
+cookies = {
+    'cookie-check': 'true',
+    'JSESSIONID': cpsUID,
+    '__direct-domain-access-fix': 'applied',
+}
 
-# print(endpointURL+"startDate="+startDate+"&endDate="+endDate+"&aggregateType="+"&includeEnhancedBilling="+enhancedBilling+"&includeMultiRegisterData="+includeRegisterData)
-URL = endpointURL+"startDate="+startDate+"&endDate="+endDate+"&aggregateType="+"&includeEnhancedBilling="+enhancedBilling+"&includeMultiRegisterData="+includeRegisterData
-# print(URL)
+headers = {
+    'Connection': 'keep-alive',
+    'DNT': '1',
+    'x-requested-with': 'XMLHttpRequest',
+    'accept-language': language,
+    'User-Agent': userAgent,
+    'opower-selected-entities': oPowerEntities,
+    'Accept': '*/*',
+    'Origin': 'https://secure.cpsenergy.com',
+    'Sec-Fetch-Site': 'cross-site',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Dest': 'empty',
+    'Referer': 'https://secure.cpsenergy.com/',
+}
 
+params = (
+    ('startDate', startDate),
+    ('endDate', endDate),
+    ('aggregateType', timeframe),
+    ('includeEnhancedBilling', enhancedBilling),
+    ('includeMultiRegisterData', 'false'),
+)
+print("cookies:\n",params)
 
-
+# response = requests.get('https://cps.opower.com/ei/edge/apis/DataBrowser-v1/cws/utilities/cps/utilityAccounts/GUID/reads', headers=headers, params=params, cookies=cookies)
+# response = requests.get('https://cps.opower.com/ei/edge/apis/DataBrowser-v1/cws/utilities/cps/utilityAccounts/GUID/reads?startDate=2021-01-29&endDate=2021-02-28&aggregateType=quarter_hour&includeEnhancedBilling=false&includeMultiRegisterData=false', headers=headers, cookies=cookies)
